08e53483d7937e3079f80314cf71fb3b63938b54
Justin <justinsotoramos2005@gmail.com>
2026-07-22 14:20:23 -0500

    actualizar

--- taller-junit-cine/taller-junit-cine/src/main/java/SistemaReservas.java
+++ taller-junit-cine/taller-junit-cine/src/main/java/SistemaReservas.java
@@ -1,1 +1,9 @@
 // Ver en enunciado: copie aquí las clases Asiento, SalaCine, NoSuchElementException y ReservaService.
+import java.util.ArrayList;
+import java.util.List;
+
+class Asiento {
+    private String codigo;
+    private String tipo;
+    private boolean ocupado;
+}
